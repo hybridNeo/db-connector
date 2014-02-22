84faaa5292bc4621c865694f7967dd1e44e1c433
hybridNeo <rahul.mahadev7@gmail.com>
2014-02-22 18:48:22 +0530

    Config files for db

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "pool"); // db user
+define('DB_PASSWORD', "pool"); // db password (mention your db password here)
+define('DB_DATABASE', "paranoid"); // database name
+define('DB_SERVER', "localhost"); // db server
+ ?>
